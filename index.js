9d74b49efe3fd406649a10566059454a6233c0dc
Shakib <alomgirhasanshakib@gmail.com>
2024-04-29 00:09:50 +0600

    impliment database intrigation and send data to the database after receiving data from client side

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
 const express = require("express");
-const { cors } = require("cors");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
-// app.use(cors);
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 500;
+
+// craftease
+// 221457ahshakib
+// mangoDB connection
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.mmdewqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const itemCollection = client.db("craft&artDB").collection("craft and art");
+
+    app.get("/addItem", async(req,res)=>{
+      const cursor = itemCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post("/addItem", async (req, res) => {
+      const newItem = req.body;
+      console.log(newItem);
+      const result = await itemCollection.insertOne(newItem);
+      res.send(result)
+    });
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("this is server");
